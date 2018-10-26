80b91f262e65f2b0cd0d9ff949c85d9956242362
wwjcn <1170317683@qq.com>
2018-10-27 00:22:31 +0800

    7-2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //注册观察者
         //User::observe(UserObserver::class);
+        Schema::defaultStringLength(191);
     }
 
     /**
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,5 +6,6 @@ $factory->define(App\Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(6),
         'content' => $faker->paragraph(10),
+        'user_id' => 2,
     ];
 });
